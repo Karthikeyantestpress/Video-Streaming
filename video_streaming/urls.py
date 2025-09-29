4bd712809a54def5e3c95020d587e548f9328813
karthikeyan <karthikeyan@testpress.in>
2025-09-29 03:24:50 -0400

    changes

--- video_streaming/urls.py
+++ video_streaming/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from video.views import video_list
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', video_list, name='home'),
     path('video/', include('video.urls')),
 ]
